947ed06dda879f416556796486447d994dccd707
Luke Bibby <lukebibby@Lukes-MacBook-Air.local>
2017-07-30 19:50:59 +1000

    Added ability to add IPv4 addresesses on router interfaces

--- router.py
+++ router.py
@@ -19,7 +19,13 @@ class RouterInterface:
     def __init__(self, intfname, intfstatus='shutdown'):
         self.intfname = intfname
         self.intfstatus = intfstatus
+        self.ipv4addr = ''
+        self.netmask = ''
 
+
+    def add_ipv4_addr(self, ipv4addr, ipv4netmask):
+        self.ipv4addr = ipv4addr
+        self.netmask = ipv4netmask
 
 class RouterRoutingTable:
     pass
@@ -39,7 +45,9 @@ rt2 = Router('Router-2')
 
 print dir(rt1)
 print dir(rt2)
-print rt2.mgmtintf.intfname
+
+rt2.mgmtintf.add_ipv4_addr('192.168.1.10','255.255.255.0')
+print rt2.mgmtintf.ipv4addr
 print rt2.mgmtintf.intfstatus
 
 
