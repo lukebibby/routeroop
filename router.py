d2393950ec9f99b57eb6a1a72a499e01e029c560
Luke Bibby <lukebibby@Lukes-MacBook-Air.local>
2017-07-30 13:55:59 +1000

    Added managmenet interface creation to Router class

--- router.py
+++ router.py
@@ -6,9 +6,13 @@ class Router:
 
     def __init__(self, rtname='Router'):
         self.rtname = rtname
+        self.mgmtintf = self.create_intf('Management0')
 
     def get_rtname(self):
         return self.rtname
+
+    def create_intf(self, intfname):
+        return RouterInterface(intfname)
 
 
 class RouterInterface:
@@ -19,3 +23,8 @@ class RouterInterface:
 
 rt1 = Router()
 rt2 = Router('Router-2')
+
+print dir(rt1)
+print dir(rt2)
+print rt2.mgmtintf.intfname
+
